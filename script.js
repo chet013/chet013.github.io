ed66960fd66f8dd176cb2ef05c225546fd8b5c01
chet013 <111523426+chet013@users.noreply.github.com>
2023-08-30 01:03:31 +0400

    Upload first version

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        dots: true,
+        infinite: false,
+        mobileFirst:true,
+        responsive: [    
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 570,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 0,
+            settings: {
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        ]
+    });   
+    
+});
+
+
+$('.intro__menu-button').click(function(){
+    $('.popup').removeClass('notActive');
+    $('body').css('overflow','hidden');
+})
+
+$('.closeModal').click(function(){
+    $('.popup').addClass('notActive');
+    $('body').css('overflow','visible');
+})
+
+$('.menu-mobile__link').click(function(){
+    $('.popup').addClass('notActive');
+    $('body').css('overflow','visible');
+})
